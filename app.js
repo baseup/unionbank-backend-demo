7e58f67feff5c537bb6731c8a392b049a709e0d6
GSPat <gabrielp.lorax@gmail.com>
2018-09-12 16:55:04 +0800

    login api

--- app.js
+++ app.js
@@ -20,6 +20,28 @@ app.get("/", (req, res) => {
   res.send("HELLO WORLD");
 });
 
+app.get("/login", (req, res) => {
+  const body = req.body;
+
+  request(
+    {
+      uri: `https://api-uat.unionbankph.com/partners/sb/convergent/v1/oauth2/authorize?client_id=${
+        body.client_id
+      }&response_type=${body.response_type}&scope=${body.scope}&redirect_uri=${
+        body.redirect_uri
+      }`,
+      method: "GET"
+    },
+    (error, response) => {
+      if (error) {
+        res.send(error);
+      } else if (response) {
+        res.send(response);
+      }
+    }
+  );
+});
+
 app.listen(app.get("port"), () => {
   console.log("Node app is running on port", app.get("port"));
 });
