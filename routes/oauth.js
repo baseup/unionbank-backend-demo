c7bc7d846fa8eb6f0a34b5beeb1e84b3e035a038
GSPat <gabrielp.lorax@gmail.com>
2018-09-13 12:36:12 +0800

    added code to redirect uri

--- routes/oauth.js
+++ routes/oauth.js
@@ -7,7 +7,42 @@ const settings = require("../lib/settings");
 const request = require("request");
 
 router.get("/redirect", function(req, res, next) {
-  res.send({});
+  //access token will be processed here
+
+  //check if there is code given
+  if (typeof req.query.code !== "undefined") {
+    //build the request
+    let options = {
+      url: `${settings.base_url}/convergent/v1/oauth2/token`, //set the url
+      headers: {
+        "content-type": "	application/x-www-form-urlencoded",
+        accept: "text/html"
+      },
+      method: "POST",
+      form: {
+        client_id: settings.client_id,
+        code: req.query.code,
+        redirect_uri:
+          "https://unionbank-backend-demo.herokuapp.com/oauth/redirect",
+        grant_type: "authorization_code"
+      }
+    };
+    request(options, function(err, response, body) {
+      if (!err) {
+        console.log("Error", err);
+      }
+      if (response) {
+        //output the result of request
+        try {
+          res.json(JSON.parse(body)); //parse the body into json
+        } catch (e) {
+          res.json(e); //if error in parsing
+        }
+      } else {
+        res.send(); //send nothing if nothing is processed
+      }
+    });
+  }
 });
 
 module.exports = router;
