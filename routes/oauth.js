e3ef5c717e17721d80d56e3c4170e5ad1d6c7150
GSPat <gabrielp.lorax@gmail.com>
2018-09-13 13:02:22 +0800

    logs response

--- routes/oauth.js
+++ routes/oauth.js
@@ -46,13 +46,9 @@ router.get("/redirect", function(req, res, next) {
 });
 
 router.get("/token", function(req, res, next) {
-  //access token will be processed here
-
-  //check if there is code given
   if (typeof req.query.code !== "undefined") {
-    //build the request
     let options = {
-      url: `${settings.base_url}/convergent/v1/oauth2/token`, //set the url
+      url: `${settings.base_url}/convergent/v1/oauth2/token`,
       headers: {
         "content-type": "	application/x-www-form-urlencoded",
         accept: "text/html"
@@ -67,18 +63,18 @@ router.get("/token", function(req, res, next) {
       }
     };
     request(options, function(err, response, body) {
+      console.log("RESPONSE: ", response);
       if (!err) {
         console.log("Error", err);
       }
       if (response) {
-        //output the result of request
         try {
-          res.json(JSON.parse(body)); //parse the body into json
+          res.json(JSON.parse(body));
         } catch (e) {
-          res.json(e); //if error in parsing
+          res.json(e);
         }
       } else {
-        res.send(); //send nothing if nothing is processed
+        res.send();
       }
     });
   }
