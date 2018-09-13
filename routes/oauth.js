4d5f31b9cf0c107564679b808097f4ab0dde5dd0
GSPat <gabrielp.lorax@gmail.com>
2018-09-13 13:07:13 +0800

    send code

--- routes/oauth.js
+++ routes/oauth.js
@@ -7,59 +7,65 @@ const settings = require("../lib/settings");
 const request = require("request");
 
 router.get("/redirect", function(req, res, next) {
-  //access token will be processed here
+  res.send({
+    code: req.query.code
+  });
+  // //access token will be processed here
 
-  //check if there is code given
-  if (typeof req.query.code !== "undefined") {
-    //build the request
-    let options = {
-      url: `${settings.base_url}/convergent/v1/oauth2/token`, //set the url
-      headers: {
-        "content-type": "	application/x-www-form-urlencoded",
-        accept: "text/html"
-      },
-      method: "POST",
-      form: {
-        client_id: settings.client_id,
-        code: req.query.code,
-        redirect_uri:
-          "https://unionbank-backend-demo.herokuapp.com/oauth/redirect",
-        grant_type: "authorization_code"
-      }
-    };
-    request(options, function(err, response, body) {
-      if (!err) {
-        console.log("Error", err);
-      }
-      if (response) {
-        //output the result of request
-        try {
-          res.json(JSON.parse(body)); //parse the body into json
-        } catch (e) {
-          res.json(e); //if error in parsing
-        }
-      } else {
-        res.send(); //send nothing if nothing is processed
-      }
-    });
-  }
+  // //check if there is code given
+  // if (typeof req.query.code !== "undefined") {
+  //   //build the request
+  //   let options = {
+  //     url: `${settings.base_url}/convergent/v1/oauth2/token`, //set the url
+  //     headers: {
+  //       "content-type": "	application/x-www-form-urlencoded",
+  //       accept: "text/html"
+  //     },
+  //     method: "POST",
+  //     form: {
+  //       client_id: settings.client_id,
+  //       code: req.query.code,
+  //       redirect_uri:
+  //         "https://unionbank-backend-demo.herokuapp.com/oauth/redirect",
+  //       grant_type: "authorization_code"
+  //     }
+  //   };
+  //   request(options, function(err, response, body) {
+  //     if (!err) {
+  //       console.log("Error", err);
+  //     }
+  //     if (response) {
+  //       //output the result of request
+  //       try {
+  //         res.json(JSON.parse(body)); //parse the body into json
+  //       } catch (e) {
+  //         res.json(e); //if error in parsing
+  //       }
+  //     } else {
+  //       res.send(); //send nothing if nothing is processed
+  //     }
+  //   });
+  // }
 });
 
 router.get("/token", function(req, res, next) {
-  if (typeof req.query.code !== "undefined") {
+  const query = req.query;
+
+  if (query.code !== "undefined") {
     let options = {
       url: `${settings.base_url}/convergent/v1/oauth2/token`,
       headers: {
-        "content-type": "	application/x-www-form-urlencoded",
-        accept: "text/html"
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json"
       },
       method: "POST",
       form: {
         client_id: settings.client_id,
-        code: req.query.code,
+        code: query.code,
         grant_type: "authorization_code"
       }
     };
+
     request(options, function(err, response, body) {
       console.log("RESPONSE: ", response);
       if (!err) {
